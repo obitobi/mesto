dca49bd80afa05e4ad6d27d053f4e8bbc5907750
Zhukov Andrew <andry-zh@yandex.ru>
2020-07-04 12:45:56 +0300

    v.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const openModalWindow = document.querySelector(".popup");
+const modalWindow = document.querySelector(".popup");
 const editProfileButton = document.querySelector(".profile__edit");
 const cancelProfileButton = document.querySelector(".popup__cancel");
 const nameValue = document.querySelector(".profile__name");
@@ -8,23 +8,23 @@ let formPopup = document.querySelector(".popup__container");
 let nameInput = document.querySelectorAll(".popup__field")[0];
 let descriptionInput = document.querySelectorAll(".popup__field")[1];
 
-function openModal() {
-    openModalWindow.classList.add('popup_opened');
-    nameInput.value = nameValue.textContent;
-    descriptionInput.value = descriptionValue.textContent;
-}
-
-function closeModal() {
-    openModalWindow.classList.remove('popup_opened');
+function onToggle() {
+    if (modalWindow.classList.contains("popup_opened")) {
+        modalWindow.classList.remove('popup_opened');
+    } else {
+        modalWindow.classList.add('popup_opened');
+        nameInput.value = nameValue.textContent;
+        descriptionInput.value = descriptionValue.textContent;
+    }
 }
 
 function submitProfile(evt) {
     evt.preventDefault();
     nameValue.textContent = nameInput.value;
     descriptionValue.textContent = descriptionInput.value;
-    openModalWindow.classList.remove('popup_opened');
+    modalWindow.classList.remove('popup_opened');
 }
 
-editProfileButton.addEventListener('click', openModal);
-cancelProfileButton.addEventListener('click', closeModal);
+editProfileButton.addEventListener('click', onToggle);
+cancelProfileButton.addEventListener('click', onToggle);
 formPopup.addEventListener('submit', submitProfile);
